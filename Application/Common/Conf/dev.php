9b56f3c595ba0521da8e0f86e9ca3925accbf536
junfei <junfei@qq.com>
2020-03-31 18:45:03 +0800

    i

--- Application/Common/Conf/dev.php
+++ Application/Common/Conf/dev.php
@@ -9,16 +9,16 @@ return array(
     'DB_HOST'   => '127.0.0.1',
     'DB_PORT'   => '3306',
     'DB_NAME'   => 'cgf_demo',
-    'DB_USER'   => 'root',
-    'DB_PWD'    => '123456',
+    'DB_USER'   => 'cgf_demo',
+    'DB_PWD'    => 'cgf_demopwd888',
     'DB_PREFIX' => 'cgf_',
 
     'log_db' => [
         'DB_TYPE'    => 'mysql',     // 数据库类型
         'DB_HOST'    => 'localhost', // 服务器地址
         'DB_NAME'    => 'log',          // 数据库名
-        'DB_USER'    => 'root',      // 用户名
-        'DB_PWD'     => '123456',          // 密码
+        'DB_USER'    => 'cgf_demo',      // 用户名
+        'DB_PWD'     => 'cgf_demopwd888',          // 密码
         'DB_PORT'    => '3306',        // 端口
         'DB_PREFIX'  => '',    // 数据库表前缀
         'DB_CHARSET' => 'utf8',      // 数据库编码默认采用utf8
